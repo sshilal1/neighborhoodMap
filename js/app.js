78f53a99b4c36c23df25b692e9ef93d8fa157f63
sshilal1 <sshilal1@binghamton.edu>
2016-04-21 23:09:01 -0400

    Added comments Open infoWindow when click on list item

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+/* ----------Neighborhood Map
+------------ Udacity Project Num 7
+------------ This project creates a map of Anchorage Alaska, and gives information
+------------ on surrouding activities/ recreation.
+----------------------------------------------------
+------------ Developed by: Stephen Shilale
+------------ 4/21/2016
+*/
+
+// Runs the 'startUp' function on window load
+window.onload = startUp;
+
+// Made the map script creation here instead of in the html
 function startUp() {
 	var mapScript = document.createElement('script');
 	mapScript.type = 'text/javascript';
@@ -5,8 +18,8 @@ function startUp() {
 	document.body.appendChild(mapScript);
 }
 
-window.onload = startUp;
-
+// Main variable housing information on the various points of interest
+// Information per POI is: Title of laoction, lat and long, address, an image, the index number, and the marker attached to the location
 var pointsOfInterest = [
 	{
 		title: 'Alaska Zoo',
@@ -14,7 +27,9 @@ var pointsOfInterest = [
 		poiLng: -149.791815,
 		streetAddr: '4731 OMalley Rd',
 		cityAddr: 'Anchorage, AK 99507',
-		imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Alaska_Zoo,_Anchorage.jpg/250px-Alaska_Zoo,_Anchorage.jpg'
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Alaska_Zoo,_Anchorage.jpg/250px-Alaska_Zoo,_Anchorage.jpg',
+		entryNum: 0,
+		gMarker: {}
 	},
 	{
 		title: 'Tony Knowles Coastal Bicycle Trail',
@@ -22,7 +37,9 @@ var pointsOfInterest = [
 		poiLng: -149.954129,
 		streetAddr: '810 W 2nd Ave',
 		cityAddr: 'Anchorage, AK 99501',
-		imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Tony_Knowles_Coastal_Trail.jpg'
+		imgSrc: 'https://upload.wikimedia.org/wikipedia/commons/e/e3/Tony_Knowles_Coastal_Trail.jpg',
+		entryNum: 1,
+		gMarker: {}
 	},
 	{
 		title: 'Alaska Railroad Depot',
@@ -30,7 +47,9 @@ var pointsOfInterest = [
 		poiLng: -149.89019,
 		streetAddr: '411 W. 1st Ave',
 		cityAddr: 'Anchorage, AK 99501',
-		imgSrc: 'https://c2.staticflickr.com/4/3294/2850221419_32d4858871.jpg'
+		imgSrc: 'https://c2.staticflickr.com/4/3294/2850221419_32d4858871.jpg',
+		entryNum: 2,
+		gMarker: {}
 	},
 	{
 		title: 'Anchorage Museam at Rasmuson Center',
@@ -38,7 +57,9 @@ var pointsOfInterest = [
 		poiLng: -149.884613,
 		streetAddr: '625 C Street',
 		cityAddr: 'Anchorage, AK 99501',
-		imgSrc: 'https://affiliations.si.edu/media_images/data/Anchorage%20Museum.jpg'
+		imgSrc: 'https://affiliations.si.edu/media_images/data/Anchorage%20Museum.jpg',
+		entryNum: 3,
+		gMarker: {}
 	},
 	{
 		title: 'Alaska Native Heritage Center',
@@ -46,7 +67,9 @@ var pointsOfInterest = [
 		poiLng: -149.717059,
 		streetAddr: '8800 Heritage Center Dr',
 		cityAddr: 'Anchorage, AK 99501',
-		imgSrc: 'http://www.alaska.org/photos/gallery3/var/albums/anchorage-photos/anchorage-attractions/Alaska-Native-Heritage-Center/Alaska-Native-Heritage-Center-03-347796285.jpg?m=1385595257'
+		imgSrc: 'http://www.alaska.org/photos/gallery3/var/albums/anchorage-photos/anchorage-attractions/Alaska-Native-Heritage-Center/Alaska-Native-Heritage-Center-03-347796285.jpg?m=1385595257',
+		entryNum: 4,
+		gMarker: {}
 	}
 ]
 
@@ -54,17 +77,23 @@ var googleAPIkey = 'AIzaSyBdPs-DH6pWE-_DYa6jKEGBYtgcWvDW6-Q';
 var yelpKey = 'R9P1G_amYFdC5Uo14SeMHw';
 var cityLatLng = {lat: 61.1881, lng: -149.90};
 
+// Creates the map. called in the Startup function
 function initMap() {
+
+	// New map centered on Anchorage
 	map = new google.maps.Map(document.getElementById('map'), {
 		center: cityLatLng,
 		zoom: 11
 	});
 
+	// General creation of infoWindow
 	infowindow = new google.maps.InfoWindow();
 
+	// Call to instantiate the markers
 	makeMarkers();
 }
 
+// This function makes the markers
 function makeMarkers() {
 	
 	for (i=0; i< pointsOfInterest.length; i++) {
@@ -83,50 +112,42 @@ function makeMarkers() {
 			title: pointsOfInterest[i].title
 		});
 
-		bindInfoWindow(marker, map, infowindow, infoContent);
-/*
-		google.maps.event.addListener(marker, 'click', (function (marker) {
-			return function () {
-				infowindow.setContent(infoContent);
-				infowindow.open(map, marker);
-			}
-		})(marker));
-
-		marker.infowindow = new google.maps.InfoWindow({
-    		content: infoContent
-		});
-
-		google.maps.event.addListener(marker,'click', function(marker) {
-			marker.infowindow.open(map, marker);
-		});
-*/
+		bindInfoWindow(marker, map, infowindow, infoContent, i);
 	}
 }
 
-function bindInfoWindow(marker, map, infowindow, html) {
+// This function creates an info window, binds it to the marker, and then sets the gMarker object as the built marker
+function bindInfoWindow(marker, map, infowindow, html, i) {
     marker.addListener('click', function() {
         infowindow.setContent(html);
         infowindow.open(map, this);
     });
+
+    // Need this to call trigger events on the marker
+    pointsOfInterest[i].gMarker = marker;
 }
 
-function setMapToPoi(newLat,newLng) {
+// This function is called when a user clicks on the list view object
+// Sets the view onto the selected location, and opens the info window
+function setMapToPoi(poi) {
 	map.setCenter( {
-		lat : newLat,
-		lng : newLng
+		lat : poi.poiLat,
+		lng : poi.poiLng
 	});
 	map.setZoom(17);
+	google.maps.event.trigger(pointsOfInterest[poi.index].gMarker, 'click');
 }
 
+// A POI object created from the array pointsOfInterest
 var Poi = function(data) {
 	this.title = ko.observable(data.title);
 	this.streetAddr = ko.observable(data.streetAddr);
 	this.poiLat = data.poiLat;
 	this.poiLng = data.poiLng;
+	this.index = data.entryNum;
 }
 
-//https://api.yelp.com/v2/search/?term=food&location=San%20Francisco,%20CA&limit=10&category_filter=pizza
-
+// The viewModel to be instantiated with knockout
 var ViewModel = function() {
 
 	var self = this;
@@ -138,7 +159,7 @@ var ViewModel = function() {
 	});
 
 	this.setPoi = function(clickedPoi) {
-		setMapToPoi(clickedPoi.poiLat,clickedPoi.poiLng);
+		setMapToPoi(clickedPoi);
 	}
 }
 
